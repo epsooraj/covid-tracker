418f7e8ef3831ea13b84b0736f2ef8b0f793c31b
Sooraj E P <epsooraj4@gmail.com>
2021-04-28 17:49:43 +0530

    Covid Tracker

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,21 +1,75 @@
 <template>
-  <div class="home">Tester</div>
+  <main v-if="!loading" class="home">
+    <DataTitle :text="title" :dataDate="dataDate" />
+    <DataBoxes :stats="stats" />
+
+    <CountrySelect @get-country="getCountryData" :countries="countries" />
+
+    <button
+      class="bg-green-700 text-white rounded p-3 mt-10 focus:outline-none hover:bg-green-600"
+      v-if="stats.Country"
+      @click="clearCountryData"
+    >
+      Clear Country
+    </button>
+  </main>
+
+  <main class="flex flex-col align-center justify-center text-center" v-else>
+    <div class="text-gray-500 text-3xl mt-10 mb-6">Fetching Data</div>
+    <img class="w-24 m-auto" :src="loadingImage" alt="" />
+  </main>
 </template>
 
 <script>
+import DataTitle from "@/components/DataTitle";
+import DataBoxes from "@/components/DataBoxes";
+import CountrySelect from "@/components/CountrySelect";
+
 export default {
   name: "Home",
-  components: {},
+  components: {
+    DataTitle,
+    DataBoxes,
+    CountrySelect,
+  },
+  data() {
+    return {
+      loading: true,
+      title: "Global",
+      dataDate: "",
+      stats: {},
+      countries: [],
+      loadingImage: require("../assets/hourglass.gif"),
+    };
+  },
   methods: {
     async fetchCovidData() {
       const res = await fetch("https://api.covid19api.com/summary");
       const data = await res.json();
       return data;
     },
+
+    getCountryData(country) {
+      this.stats = country;
+      this.title = country.Country;
+    },
+
+    async clearCountryData() {
+      this.loading = true;
+      await this.fetchAndUpdateCovidData();
+    },
+
+    async fetchAndUpdateCovidData() {
+      const data = await this.fetchCovidData();
+      this.dataDate = data.Date;
+      this.stats = data.Global;
+      this.countries = data.Countries;
+      this.loading = false;
+    },
   },
-  async created() {
-    const data = await this.fetchCovidData();
-    console.log(data);
+
+  created() {
+    this.fetchAndUpdateCovidData();
   },
 };
 </script>
